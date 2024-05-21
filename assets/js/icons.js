05bf7ebe4e709c70024efa693639a0aae75e6ce4
ghianluca <ribeiroghian@gmail.com>
2024-05-21 20:03:26 -0300

    feat: add language switcher, update footer, remove and update sections

---
+++ assets/js/icons.js
@@ -0,0 +1,108 @@
+const translations = {
+    en: {
+        home: "HOME",
+        about: "ABOUT ME",
+        aboutMe: "ABOUT ME<span class='color'>:</span>",
+        experiences: "EXPERIENCES",
+        portfolio: "PORTFOLIO",
+        contact: "CONTACT",
+        intro: "Hello there, welcome to my website! My name is Rafael Nobre, I'm a designer graduated from UFPel and I work in various areas of design and communication. Ok, but what is Elefante, right?! <br><br>Elefante was a communication studio that I opened in Porto Alegre with a partner where we worked together for 3 years. Currently, it is a personal project that I maintain, I do freelance projects and promote my work. A few years ago, I fully shifted my career to digital, where I have been working as a UX and UI Designer, working in technology and software development companies. <br><br>Feel free to learn a little more about my work!",
+        jobs: "WHERE I WORKED<span class='color'>:</span>",
+        introTwo: "<span style='color: rgb(255, 255, 255);'><br><br>I create designs for various media. <br> From the discovery process to <br>problem solving.</span>",
+        experiencesTwo: "EXPERIENCES<span class='color'>_</span>",
+        introThree: "I think I've become what they call a <span class='color'><a href='https://medium.com/chocoladesign/designer-unic%C3%B3rnio-existe-ou-n%C3%A3o-sim-85b59c411789' target='_blank'><strong>unicorn designer.</strong></a></span> I've worked on various projects, but what I have the most experience in and enjoy doing the most are UI, UX, and Branding. Any questions? <span class='color'><a href='#contato'><strong>Get in touch!</strong></a></span>",
+        digitalTitle: "DIGITAL",
+        digitalContent: "Website <span class='color'> / </span> Hotsite <span class='color'> / </span> Interface <span class='color'> / </span> App <span class='color'> / </span> E-commerce <span class='color'> / </span> Presentation <span class='color'> / </span> Newsletter <span class='color'> / </span> Email marketing",
+        graphicTitle: "GRAPHIC AND EDITORIAL",
+        graphicContent: "Flyer <span class='color'> / </span> Poster <span class='color'> / </span> Sticker <span class='color'> / </span> Banner <span class='color'> / </span> Ad <span class='color'> / </span> Billboard <span class='color'> / </span> Point of sale <span class='color'> / </span> Packaging <span class='color'> / </span> Newspaper <span class='color'> / </span> Catalog <span class='color'> / </span> Electronic publication",
+        uxTitle: "UX AND DESIGN THINKING",
+        uxContent: "Empathy map <span class='color'> / </span> Persona creation <span class='color'> / </span> UX Research <span class='color'> / </span> UX Writing <span class='color'> / </span> Flow creation <span class='color'> / </span> Analog research",
+        identityTitle: "VISUAL IDENTITY",
+        identityContent: "Logo <span class='color'> / </span> Business card <span class='color'> / </span> Stationery and brand applications <span class='color'> / </span> Brand use guide <span class='color'> / </span> Brandbook",
+        brandingTitle: "BRANDING",
+        brandingContent: "Naming <span class='color'> / </span> Logo <span class='color'> / </span> Brand positioning and management <span class='color'> / </span> Tone of voice <span class='color'> / </span> Social media <span class='color'> / </span> Market study",
+        footerTitle: "WHO AM I",
+        footerName: "Rafael Nobre",
+        footerText: "I have been working with design for over 14 years, helping companies maximize their revenues by making thoughts tangible through structured processes.",
+        footerAddress: "Porto Alegre, RS, Brazil<br><a href='mailto:elefante@elefante.me'>nobrepires@gmail.com</a><br><abbr title='Phone'></abbr>+55 51 993618932"
+    },
+    pt: {
+        home: "HOME",
+        about: "SOBRE MIM",
+        aboutMe: "SOBRE MIM<span class='color'>:</span>",
+        experiences: "EXPERIÊNCIAS",
+        portfolio: "PORTFOLIO",
+        contact: "CONTATO",
+        intro: "Olá pessoa, seja bem-vinda ao meu site! Me chamo Rafael Nobre, sou designer formado pela UFPel e trabalho em diversas áreas do design e da comunicação. Ok, mas e o que é Elefante, certo?! <br><br>Elefante foi um estúdio de comunicação que abri em Porto Alegre com um sócio onde trabalhamos juntos por 3 anos. Atualmente é um projeto pessoal que mantenho, faço projetos como freelancer e divulgo meus trabalhos. Há alguns anos voltei minha carreira totalmente para o digital, onde tenho atuado como UX e UI Designer, trabalhando em empresas de tecnologia e desenvolvimento de software. <br><br>Fique à vontade para conhecer um pouco mais do meu trabalho!",
+        jobs: "ONDE TRABALHEI<span class='color'>:</span>",
+        introTwo: "<span style='color: rgb(255, 255, 255);'><br><br>Crio design para diversas mídias. <br> Do processo de descobrimento à resolução do problema.</span>",
+        experiencesTwo: "EXPERIÊNCIAS<span class='color'>_</span>",
+        introThree: "Acho que me tornei aquilo que chamam de <span class='color'><a href='https://medium.com/chocoladesign/designer-unic%C3%B3rnio-existe-ou-n%C3%A3o-sim-85b59c411789' target='_blank'><strong>designer unicórnio.</strong></a></span> Já trabalhei com diversos projetos, mas o que mais tenho experiência e gosto de fazer é nas áreas e UI, UX e Branding. Dúvidas? <span class='color'><a href='#contato'><strong>Entre em contato!</strong></a></span>",
+        digitalTitle: "DIGITAL",
+        digitalContent: "Website <span class='color'> / </span> Hotsite <span class='color'> / </span> Interface <span class='color'> / </span> Aplicativo <span class='color'> / </span> Loja virtual <span class='color'> / </span> Apresentação <span class='color'> / </span> Newsletter <span class='color'> / </span> E-mail marketing",
+        graphicTitle: "GRÁFICO E EDITORIAL",
+        graphicContent: "Flyer <span class='color'> / </span> Cartaz <span class='color'> / </span> Adesivo <span class='color'> / </span> Banner <span class='color'> / </span> Anúncio <span class='color'> / </span> Outdoor <span class='color'> / </span> Ponto-de-venda <span class='color'> / </span> Embalagem <span class='color'> / </span> Jornal <span class='color'> / </span> Catálogo <span class='color'> / </span> Publicação eletrônica",
+        uxTitle: "UX E DESIGN THINKING",
+        uxContent: "Mapa de empatia <span class='color'> / </span> Criação de Personas <span class='color'> / </span> UX Research <span class='color'> / </span> UX Writing <span class='color'> / </span> Criação de fluxos <span class='color'> / </span> Pesquisas análogas",
+        identityTitle: "IDENTIDADE VISUAL",
+        identityContent: "Logo <span class='color'> / </span> Cartão de visita <span class='color'> / </span> Papelaria e aplicações da marca <span class='color'> / </span> Guia de uso da marca <span class='color'> / </span> Brandbook",
+        brandingTitle: "BRANDING",
+        brandingContent: "Naming <span class='color'> / </span> Logo <span class='color'> / </span> Posicionamento e Gerenciamento de Marca <span class='color'> / </span> Tom de Voz <span class='color'> / </span> Redes sociais <span class='color'> / </span> Estudo de mercado",
+        footerTitle: "QUEM SOU EU",
+        footerName: "Rafael Nobre",
+        footerText: "Trabalho com design há mais de 14 anos, ajudando empresas a maximizar suas receitas, tangibilizando o pensamento através de processos estruturados.",
+        footerAddress: "Porto Alegre, RS, Brasil<br><a href='mailto:elefante@elefante.me'>nobrepires@gmail.com</a><br><abbr title='Telefone'></abbr>+55 51 993618932"
+    }
+};
+
+function changeLanguage(language) {
+    document.querySelectorAll('[data-translate]').forEach(element => {
+        const key = element.getAttribute('data-translate');
+        element.innerHTML = translations[language][key];
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const brazilIcons = document.querySelectorAll('.icon-brazil');
+    const usaIcons = document.querySelectorAll('.icon-usa');
+
+    // Inicialização dos ícones
+    brazilIcons.forEach(icon => icon.classList.add('color'));
+    usaIcons.forEach(icon => icon.classList.add('bw'));
+
+    function toggleLanguage(lang) {
+        if (lang === 'pt') {
+            brazilIcons.forEach(icon => {
+                icon.classList.add('color');
+                icon.classList.remove('bw');
+            });
+            usaIcons.forEach(icon => {
+                icon.classList.add('bw');
+                icon.classList.remove('color');
+            });
+            changeLanguage('pt');
+        } else {
+            usaIcons.forEach(icon => {
+                icon.classList.add('color');
+                icon.classList.remove('bw');
+            });
+            brazilIcons.forEach(icon => {
+                icon.classList.add('bw');
+                icon.classList.remove('color');
+            });
+            changeLanguage('en');
+        }
+    }
+
+    brazilIcons.forEach(icon => {
+        icon.addEventListener('click', function() {
+            toggleLanguage('pt');
+        });
+    });
+
+    usaIcons.forEach(icon => {
+        icon.addEventListener('click', function() {
+            toggleLanguage('en');
+        });
+    });
+});
